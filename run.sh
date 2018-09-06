9e2bbcc2d19dfdfd6556634d94c43d81dd85833f
ianagbip1oti <ianagbip1oti@gmail.com>
2018-09-06 23:33:48 +0000

    Refator extra_args for specific environments

--- run.sh
+++ run.sh
@@ -8,10 +8,16 @@ workspace=${2-~/codemagic}
 docker build -f Dockerfile.base . -t codemagic:base
 docker build -f Dockerfile.$image . -t codemagic:$image
 
+case $image in 
+  java*)
+    extra_args="-v $workspace/.m2:/home/hacker/.m2"
+    ;;
+esac
+
 docker run -it \
   -v $workspace:/workspace \
-  -v $workspace/.m2:/home/hacker/.m2 \
   -e "TERM=xterm-256color" \
+  $extra_args \
   --user=hacker \
   --hostname="$image.codemagic" \
   codemagic:$image
